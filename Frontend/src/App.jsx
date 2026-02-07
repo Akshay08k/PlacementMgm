b5f4096fcee62264ded62890fcd925056397fc8b
akshay <akshaykomade012345@gmail.com>
2026-02-07 13:09:01 +0530

    Added AboutUs On FE

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SignInPage from './components/Login'
-
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Homepage from './components/Homepage'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import SignInPage from "./components/Login";
+import AboutUs from "./components/Aboutus";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route Component={Homepage} path='/'></Route>
-          <Route Component={SignInPage} path='/signin'></Route>
+          <Route Component={Homepage} path="/"></Route>
+          <Route Component={SignInPage} path="/signin"></Route>
+          <Route Component={AboutUs} path="/aboutus"></Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
